344880a359ba6d1986be369c4a8aa5d35406b820
alexaniatoma <alexaniatoma@hotmail.com>
2026-09-15 17:37:55 -0300

    chore: inicia estrutura do site da clínica Essenza

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+     <Navbar />
+        <Routes>       
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>    
+    
+  )
+}
+
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return(
+        <div>
+            <h1>Está funcionando</h1>
+        </div>
+    )
+}
